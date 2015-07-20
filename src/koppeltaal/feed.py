cff48eac78687c43f52aa409524145020f34d4a5
Jan-Jaap Driessen <jdriessen@minddistrict.com>
2015-07-20 14:25:21 +0200

    code comments, let's do more of those

--- src/koppeltaal/feed.py
+++ src/koppeltaal/feed.py
@@ -1,9 +1,8 @@
 import koppeltaal
 
-# XXX Move to feedparser.
+# Move this parsing to the python package "feedparser".
 def find_link(entry):
     # Ugly python, need to escape the {} to use .format().
     for link in entry._xml.iterchildren(tag='{%(atom)s}link' % koppeltaal.NS):
         if link.attrib.get('rel') == 'self':
             return link.attrib['href']
-
