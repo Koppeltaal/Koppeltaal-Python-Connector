8101d29f79a28ba4fbd52201bb9b5fcf9cb62897
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2016-02-18 12:13:21 +0100

    move more stuff to interfaces and introduce a logger module; essentially clearing out stuff from __init__.py

---
+++ src/koppeltaal/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+
+logger = logging.getLogger('koppeltaal.connector')
+
+
+critical = logger.critical
+debug = logger.debug
+error = logger.error
+info = logger.info
+warn = logger.warn
