cfc8f3b0fb0196f18fc75540428c80e47277b9cf
Jan-Wijbrand Kolman <jw@fietsventiel.local>
2016-08-30 13:20:05 +0200

    we are still testing against edge, we will deal with different versions in testing later

--- src/koppeltaal/tests/test_integration.py
+++ src/koppeltaal/tests/test_integration.py
@@ -7,7 +7,7 @@ def test_request_metadata(connector):
     result = connector.metadata()
     assert isinstance(result, dict)
     assert result.get('name') == 'Koppeltaal'
-    assert result.get('version') == 'v1.0'
+    assert result.get('version') == 'v1.1.1'  # edge.
     assert result.get('fhirVersion') == '0.0.82'
 
 
